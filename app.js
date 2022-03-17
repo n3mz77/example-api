967fd0ccc223e5c0c5e207353e7bb24690ba85fc
wattana <wattana.php@gmail.com>
2022-03-17 16:40:55 +0700

    add docs on homepage. update json data. remove unused code

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 
 const categoryRouter = require('./routes/category');
 const productsRouter = require('./routes/products');
+const indexRouter = require('./routes/index');
 
 const app = express();
 
@@ -18,6 +19,7 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', indexRouter);
 app.use('/category', categoryRouter);
 app.use('/products', productsRouter);
 
